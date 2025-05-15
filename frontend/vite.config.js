bf407163ee352e6e5a9640577b9ed18cd4fee01c
Pavan Teja <143066166+pavanteja18@users.noreply.github.com>
2025-05-15 19:42:19 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  build: {
-    outDir: path.resolve(__dirname, "../backend/frontend"), // ✅ Match Express static path
-    emptyOutDir: true,
-  },
+	plugins: [react()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:3000",
+			},
+		},
+	},
 });
