7fdf4a339c503ecbc2c3238341b1a75de6fb906c
Pavan Teja <143066166+pavanteja18@users.noreply.github.com>
2025-05-15 18:33:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -34,12 +34,11 @@ app.use("/api/entries", router);
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "/frontend/dist");
-
+  const frontendPath = path.join(__dirname, "..", "frontend", "dist");
   app.use(express.static(frontendPath));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
+    res.sendFile(path.resolve(frontendPath, "index.html"));
   });
 }
 
